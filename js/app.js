ea0237ca55c48c88b92dfaf8d0e00feb52a87cc9
Misha office <mikhail@devrepublic.nl>
2019-05-03 10:36:36 +0200

    1st commit

---
+++ js/app.js
@@ -0,0 +1,73 @@
+class UI {
+  constructor() {
+    this.budgetFeedback = document.querySelector(".budget-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.budgetForm = document.getElementById("budget-form");
+    this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("budget-amount");
+    this.expenseAmount = document.getElementById("expense-amount");
+    this.balance = document.getElementById("balance");
+    this.balanceAmount = document.getElementById("balance-amount");
+    this.expenseForm = document.getElementById("expense-form");
+    this.expenseInput = document.getElementById("expense-input");
+    this.amountInput = document.getElementById("amount-input");
+    this.expenseList = document.getElementById("expense-list");
+    this.itemList = [];
+    this.itemID = 0;
+  }
+  //submit budeget method
+  submitBudgetForm(){
+    const value = this.budgetInput.value;
+    if(value === "" || value < 0){
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+      const self = this;
+      console.log(this);
+
+      setTimeout(function(){
+        self.budgetFeedback.classList.remove('showItem');
+      }, 4000);
+    }else{
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = '';
+      this.showBalance();
+    }
+  }
+  //show balance
+  showBalance(){
+   const expense = this.totalExpense();
+   const total = parseInt(this.budgetAmount.text) - expense; 
+   this.balanceAmount.textContent = total;
+  }
+  //total expense
+  totalExpense(){
+    let total = 400;
+    return total;
+  }
+}
+
+function eventListeners(){
+  const budgetForm = document.getElementById('budget-form');
+  const expenceForm = document.getElementById('expence-form');
+  const expenceList = document.getElementById('expence-list');
+
+  //new instance of UI CLASS
+  const ui = new UI()
+
+  //budget form submit
+  budgetForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    ui.submitBudgetForm();
+  })
+  
+   //expence form submit
+   budgetForm.addEventListener('submit', function(event){
+    event.preventDefault();
+  })
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  eventListeners();
+});
